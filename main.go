1bd03e5d52923deaaa2b906b1d6ace6f001895a2
yizhixiaokong <wdhxx20121@126.com>
2020-06-11 21:25:10 +0800

    schedule 2020-06-11

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const ptrbg string = "Phantom-Thunder-Remake-by-Golang"
 
 func main() {
 
 	fmt.Println("hello!", ptrbg, "!")
+	fmt.Println("feel bed bed :(")
 }
